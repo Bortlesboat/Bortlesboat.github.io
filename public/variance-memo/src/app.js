3b8f8f266fd63d301a93b880a1031b6d00bd35b7
Andrew Barnes <bortstheboat@gmail.com>
2026-05-18 00:47:35 -0400

    Bust cached Variance Memo assets

--- public/variance-memo/src/app.js
+++ public/variance-memo/src/app.js
@@ -1,4 +1,4 @@
-import { analyzeCsvText, analyzeFile } from "./variance.js";
+import { analyzeCsvText, analyzeFile } from "./variance.js?v=043b845";
 
 const sampleCsv = `Account,Department,Category,Period,Actual,Budget,Forecast,Prior Year
 SaaS recurring revenue,Sales,Revenue,Jan 2026,"$120,000","$100,000","$245,000","$92,000"
